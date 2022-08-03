5924ed23d9249ac304e135a08d52e4d1794a6d2e
Daniel Diniz <daniel.diniz1@icloud.com>
2022-08-02 22:17:34 -0300

    refatorando para controller injetar a interface e não a classe especifica

--- src/main/java/br/com/financial/transacoes/controller/ReceitaController.java
+++ src/main/java/br/com/financial/transacoes/controller/ReceitaController.java
@@ -2,9 +2,7 @@ package br.com.financial.transacoes.controller;
 
 import br.com.financial.transacoes.controller.dto.TransacaoCriadaDTO;
 import br.com.financial.transacoes.controller.form.ReceitaForm;
-import br.com.financial.transacoes.model.enums.Categoria;
-import br.com.financial.transacoes.model.enums.Conta;
-import br.com.financial.transacoes.service.DefaultReceitaService;
+import br.com.financial.transacoes.service.TransacaoService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,20 +16,14 @@ import javax.validation.Valid;
 public class ReceitaController {
 
     @Autowired
-    private DefaultReceitaService receitaService;
+    private TransacaoService transacaoService;
 
     private static final Logger LOGGER = LogManager.getLogger(ReceitaController.class);
 
     @PostMapping("/adicionar-receita")
     @ResponseBody
     public ResponseEntity<TransacaoCriadaDTO> cadastroReceita(@RequestBody @Valid ReceitaForm receitaForm){
-        LOGGER.info("Valor R$ " + receitaForm.getValor());
-        LOGGER.info("Descrição: " + receitaForm.getDescricao());
-        LOGGER.info("Data de Lançamento: " + receitaForm.getDataLancamento());
-        LOGGER.info("Categoria: " + Categoria.toEnumCategoria(Integer.parseInt(receitaForm.getCategoria())));
-        LOGGER.info("Conta " + Conta.toEnumConta(Integer.parseInt(receitaForm.getConta())));
-
-        TransacaoCriadaDTO transacaoCriadaDTO = receitaService.adicionar(receitaForm);
+        TransacaoCriadaDTO transacaoCriadaDTO = transacaoService.adicionar(receitaForm);
 
         return ResponseEntity.status(202).body(transacaoCriadaDTO);
     }
